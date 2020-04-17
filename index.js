7692c71cb405dd91fc49b0d14347e510b9aa25b0
Jai <jaikumarsingh1908@gmail.com>
2020-04-17 19:13:02 +0530

    Express Morgan Serve static file

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const http = require('http');
+const morgan = require('morgan');
 
 const hostname = 'localhost';
 const port = 3000;
@@ -7,9 +8,13 @@ const port = 3000;
 const app = express();/*we are saying that our application is going to use the express nodemodule
 once we do that, then Express provides a bunch of methods that we can use to construct our web server. */
 
+app.use(morgan('dev'));//using developer morgan
+
+//seting up server to use html files in public folder
+app.use(express.static(__dirname+'/public'))//serve static file from directory name public in root folder
 //setting up server 
 app.use((req,res,next/*(next is optional)*/) => {
-    console.log(req.headers);
+
     res.statusCode=200;
     res.setHeader('Content-Type','text/html');
     res.end('<html><body><h1>Express Server</h1></body></html>')
